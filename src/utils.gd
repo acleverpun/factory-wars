30694ba26fc10695e0cbf589cc07b64ffc221bb1
Ross Hadden <rosshadden@gmail.com>
2020-05-02 01:02:48 -0400

    Added my utils file from my unfinished TankerZ game that this is essentially replacing.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

---
+++ src/utils.gd
@@ -0,0 +1,24 @@
+static func isClick(event, button = 1):
+	return (
+		event is InputEventMouseButton and
+		event.button_index == button and
+		event.is_pressed()
+	)
+
+static func hasPoint(polygon, point):
+	var verts = polygon.get_polygon()
+	var isInside = false;
+
+	for v in range(verts.size()):
+		verts[v] = verts[v] + polygon.position
+
+	var vert2 = verts[-1]
+	for vert in verts:
+		var intersects = (
+			(vert.y > point.y) != (vert2.y > point.y) &&
+			point.x < (vert2.x - vert.x) * (point.y - vert.y) / (vert2.y - vert.y) + vert.x
+		)
+		if intersects: isInside = !isInside
+		vert2 = vert
+
+	return isInside
