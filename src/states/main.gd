19ef5b002dfde4a2aa665f81b7c7a60cc7e67f99
Ross Hadden <rosshadden@gmail.com>
2020-05-02 01:05:23 -0400

    Print on polygon click.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/states/main.gd
+++ src/states/main.gd
@@ -1,12 +1,23 @@
 extends ViewportContainer
 
+const utils = preload('res://src/utils.gd')
+
 onready var Map = preload("res://src/maps/lab.tscn")
 
+var map
+
 func _ready():
-	var map = Map.instance()
+	map = Map.instance()
 	map.name = "map"
 	add_child(map)
 
 func _input(event):
 	if Input.is_action_pressed("game.close"): get_tree().quit()
-	if Input.is_action_pressed("game.restart"): get_tree().reload_current_scene()
+	elif Input.is_action_pressed("game.restart"): get_tree().reload_current_scene()
+
+	if utils.isClick(event):
+		var nodes = map.get_children()
+
+		for node in nodes:
+			if utils.hasPoint(node, event.get_position()):
+				print(node.name)
