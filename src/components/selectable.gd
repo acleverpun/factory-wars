8e31a89b6bb5a548f44ef42100eb9205259d6774
Ross Hadden <rosshadden@gmail.com>
2020-05-14 02:29:41 -0400

    Moved click handling to local selectable component.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/components/selectable.gd
+++ src/components/selectable.gd
@@ -1,31 +1,19 @@
-extends Node
+extends Area2D
 
 signal select
 signal deselect
 
 onready var parent := get_parent()
 
-var selected := false
-
 func _ready():
+	connect("input_event", self, "onInputEvent")
+
 	if parent.has_method("onSelect"): connect("select", parent, "onSelect")
 	if parent.has_method("onDeselect"): connect("deselect", parent, "onDeselect")
 
-func select():
-	# TODO: letting node always be selected for dev
-	selected = false
+func onInputEvent(viewport: Viewport, event: InputEvent, shapeIndex: int):
+	if not event is InputEventMouseButton: return
+	if not event.pressed: return
 
-	if not selected:
-		selected = true
+	if event.button_index == 1:
 		emit_signal("select")
-	return selected
-
-func deselect():
-	if selected:
-		selected = false
-		emit_signal("deselect")
-	return selected
-
-func toggle():
-	if selected == true: return deselect()
-	return select()
