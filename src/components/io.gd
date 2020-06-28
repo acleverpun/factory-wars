e03cfdeed005e2ce6d95bf72d63a9faa74f0d49d
Ross Hadden <rosshadden@gmail.com>
2020-06-28 16:23:12 -0400

    Added IO component, with bit flags for inputs and outputs.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

---
+++ src/components/io.gd
@@ -0,0 +1,7 @@
+extends Polygon2D
+
+export(int, FLAGS, "North", "East", "South", "West") var inputs = 0
+export(int, FLAGS, "North", "East", "South", "West") var outputs = 0
+
+func _ready():
+	pass
