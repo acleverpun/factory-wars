0304a5ca0a6745777c577912b28dafcef2be9a18
Ross Hadden <rosshadden@gmail.com>
2020-07-02 01:06:22 -0400

    Handled output rotation perfectly.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/components/io.gd
+++ src/components/io.gd
@@ -8,14 +8,6 @@ const EAST = 1 << 1
 const SOUTH = 1 << 2
 const WEST = 1 << 3
 
-const center = Vector2(8, 8)
-const up = Vector2(0, -16)
-const down = Vector2(0, 16)
-const left = Vector2(-16, 0)
-const right = Vector2(16, 0)
-
-const inputColor = Color8(255, 255, 0, 255)
-
 export(int, FLAGS, "North", "East", "South", "West") var inputs = 0
 export(int, FLAGS, "North", "East", "South", "West") var outputs = 0
 
@@ -26,15 +18,6 @@ func _ready():
 	pass
 
 func _draw():
-	if inputs & NORTH:
-		drawArrow(-PI/2, IOType.INPUT)
-	if inputs & SOUTH:
-		drawArrow(PI/2, IOType.INPUT)
-	if inputs & WEST:
-		drawArrow(PI, IOType.INPUT)
-	if inputs & EAST:
-		drawArrow(0, IOType.INPUT)
-
 	if outputs & NORTH:
 		drawArrow(-PI/2, IOType.OUTPUT)
 	if outputs & SOUTH:
@@ -44,10 +27,22 @@ func _draw():
 	if outputs & EAST:
 		drawArrow(0, IOType.OUTPUT)
 
+	if inputs & NORTH:
+		drawArrow(-PI/2, IOType.INPUT)
+	if inputs & SOUTH:
+		drawArrow(PI/2, IOType.INPUT)
+	if inputs & WEST:
+		drawArrow(PI, IOType.INPUT)
+	if inputs & EAST:
+		drawArrow(0, IOType.INPUT)
+
 func drawArrow(angle: float, ioType: int):
 	var color = Color.orange if ioType == IOType.INPUT else Color.green
 	var arrow = Arrow.instance()
 	arrow.rotate(angle)
-	arrow.color = color
+	arrow.find_node("body").color = color
+
+	if ioType == IOType.INPUT:
+		arrow.find_node("center").rotate(PI)
 
 	add_child(arrow)
