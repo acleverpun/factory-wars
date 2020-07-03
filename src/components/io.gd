01732f7753b9207796424df8bc18d5eddf086ac2
Ross Hadden <rosshadden@gmail.com>
2020-07-03 11:55:32 -0400

    Changed enum casing.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/components/io.gd
+++ src/components/io.gd
@@ -4,43 +4,43 @@ extends Polygon2D
 onready var Arrow := preload("res://src/gui/arrow.tscn")
 
 # TODO: move to universal storage
-enum IOType { INPUT, OUTPUT }
+enum IOType { Input, Output }
 
 # TODO: move to universal storage
 enum Direction {
-	EAST = 1 << 0,
-	SOUTH = 1 << 1,
-	WEST = 1 << 2,
-	NORTH = 1 << 3,
+	East = 1 << 0,
+	South = 1 << 1,
+	West = 1 << 2,
+	North = 1 << 3,
 }
 
 export(Direction, FLAGS) var inputs = 0
 export(Direction, FLAGS) var outputs = 0
 
 onready var sides = {
-	Direction.EAST: null,
-	Direction.SOUTH: null,
-	Direction.WEST: null,
-	Direction.NORTH: null,
+	Direction.East: null,
+	Direction.South: null,
+	Direction.West: null,
+	Direction.North: null,
 }
 
 func _draw():
 	var d := 0
 	for dir in Direction:
 		if outputs & Direction[dir]:
-			drawArrow(d * PI/2, IOType.OUTPUT)
+			drawArrow(d * PI/2, IOType.Output)
 		if inputs & Direction[dir]:
-			drawArrow(d * PI/2, IOType.INPUT)
+			drawArrow(d * PI/2, IOType.Input)
 		d += 1
 
 func drawArrow(angle: float, ioType: int):
-	var color := Color.tomato if ioType == IOType.INPUT else Color.lawngreen
+	var color := Color.tomato if ioType == IOType.Input else Color.lawngreen
 	var arrow := Arrow.instance()
 	color.a = 0.8
 	arrow.rotate(angle)
 	arrow.find_node("body").color = color
 
-	if ioType == IOType.INPUT:
+	if ioType == IOType.Input:
 		arrow.find_node("center").rotate(PI)
 
 	add_child(arrow)
