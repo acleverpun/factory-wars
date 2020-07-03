df89a691fdff87aeea235e58bba1a0c011e3307a
Ross Hadden <rosshadden@gmail.com>
2020-07-03 03:26:01 -0400

    Generalized directions using maths.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/components/io.gd
+++ src/components/io.gd
@@ -1,37 +1,28 @@
 tool
 extends Polygon2D
 
-export(int, FLAGS, "North", "East", "South", "West") var inputs = 0
-export(int, FLAGS, "North", "East", "South", "West") var outputs = 0
+export(int, FLAGS, "east", "north", "west", "south") var inputs = 0
+export(int, FLAGS, "east", "north", "west", "south") var outputs = 0
 
 onready var Arrow := preload("res://src/gui/arrow.tscn")
 
 enum IOType { INPUT, OUTPUT }
 
+# TODO: move to universal storage
 const NONE = 0
-const NORTH = 1
-const EAST = 1 << 1
-const SOUTH = 1 << 2
-const WEST = 1 << 3
+const EAST = 1 << 0
+const NORTH = 1 << 1
+const WEST = 1 << 2
+const SOUTH = 1 << 3
+
+const directions = [ EAST, NORTH, WEST, SOUTH ]
 
 func _draw():
-	if outputs & NORTH:
-		drawArrow(-PI/2, IOType.OUTPUT)
-	if outputs & SOUTH:
-		drawArrow(PI/2, IOType.OUTPUT)
-	if outputs & WEST:
-		drawArrow(PI, IOType.OUTPUT)
-	if outputs & EAST:
-		drawArrow(0, IOType.OUTPUT)
-
-	if inputs & NORTH:
-		drawArrow(-PI/2, IOType.INPUT)
-	if inputs & SOUTH:
-		drawArrow(PI/2, IOType.INPUT)
-	if inputs & WEST:
-		drawArrow(PI, IOType.INPUT)
-	if inputs & EAST:
-		drawArrow(0, IOType.INPUT)
+	for d in len(directions):
+		if outputs & directions[d]:
+			drawArrow(d * PI/2, IOType.OUTPUT)
+		if inputs & directions[d]:
+			drawArrow(d * PI/2, IOType.INPUT)
 
 func drawArrow(angle: float, ioType: int):
 	var color := Color.tomato if ioType == IOType.INPUT else Color.lawngreen
