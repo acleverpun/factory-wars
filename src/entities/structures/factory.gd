80302f27135c533775e8662e4064192632d53512
Ross Hadden <rosshadden@gmail.com>
2020-07-03 01:16:27 -0400

    Made factory require and spend resources to build.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/entities/structures/factory.gd
+++ src/entities/structures/factory.gd
@@ -2,12 +2,20 @@ extends Polygon2D
 
 const Unit = preload("res://src/entities/units/unit.tscn")
 
+export var value := 0
+
 onready var parent = get_parent()
 
 func onSelect():
 	spawn()
 
 func spawn():
+	if value < 20:
+		prints("value not high enough", value)
+		value += 10
+		return
+
+	value -= 20
 	var unit = Unit.instance()
 	unit.position = self.position + Vector2(0, 32)
 	parent.add_child(unit)
