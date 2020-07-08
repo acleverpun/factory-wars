bc3fcadb4c6934fd6b866b4e293d3f792619344b
Ross Hadden <rosshadden@gmail.com>
2020-07-08 19:49:38 -0400

    Transformed main scene wrapper into an autoloaded mappings system instead...
    - makes all scenes use it (which is convenient for running individual scenes directly)
    - lets me make the default scene the actual scene I'm working on, sanding out a rough edge
    
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/systems/mappings.gd
+++ src/systems/mappings.gd
@@ -1,13 +1,4 @@
-extends ViewportContainer
-
-onready var Map := preload("res://src/maps/lab.tscn")
-
-var map: Node
-
-func _ready():
-	map = Map.instance()
-	map.name = "map"
-	add_child(map)
+extends Node
 
 func _input(event: InputEvent):
 	if Input.is_action_pressed("game.close"): get_tree().quit()
