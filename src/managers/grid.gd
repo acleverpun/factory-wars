f59a674fa96f970e27687430faf03d1a7f63a137
Ross Hadden <rosshadden@gmail.com>
2020-07-20 23:53:13 -0400

    Drew grid.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/managers/grid.gd
+++ src/managers/grid.gd
@@ -14,6 +14,8 @@ func _ready():
 	for child in get_children():
 		layers[child.name] = child
 
+	drawGrid()
+
 func addNode(Class: Resource, layerName: String, position: Vector2):
 	assert(layers.has(layerName))
 	var layer: TileMap = layers[layerName] as TileMap
@@ -33,3 +35,17 @@ func setData(layerName: String, position: Vector2, value: int) -> void:
 	var layer: TileMap = layers[layerName] as TileMap
 	var gridPosition := layer.world_to_map(position)
 	layer.set_cellv(gridPosition, value)
+
+func drawGrid():
+	var viewport: Viewport = $"/root"
+	for x in range(0, viewport.size.x / size + 1):
+		drawLine(Vector2(x * size, 0), Vector2(x * size, viewport.size.y))
+	for y in range(0, viewport.size.y / size + 1):
+		drawLine(Vector2(0, y * size), Vector2(viewport.size.x, y * size))
+
+func drawLine(from, to):
+	var line: Line2D = Line2D.new()
+	line.width = 1
+	line.default_color = ColorN("gray", 0.25)
+	line.points = PoolVector2Array([ from, to ])
+	self.add_child(line)
