b9bac411646b2fee5697dd1f965fd48bdebfc0b9
Ross Hadden <rosshadden@gmail.com>
2020-08-27 23:56:49 -0400

    Renamed structures => buildings.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/systems/modes/select.gd
+++ src/systems/modes/select.gd
@@ -3,13 +3,15 @@ extends Node
 var value
 
 onready var fsm: Fsm = get_parent()
+onready var grid: Grid = fsm.get_parent().get_node("grid")
 
 func _ready() -> void:
 	connect("input_event", self, "_onInputEvent")
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("game.action"):
-		prints("action")
+		var data := grid.getAllData(event.position)
+		prints(data)
 
 	if Input.is_action_just_pressed("game.context"):
 		prints("context")
