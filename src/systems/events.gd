43f5ec242c5a4de57b28ce2875ea258f38dac143
Ross Hadden <rosshadden@gmail.com>
2021-01-27 17:06:03 -0500

    Misc backports.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/systems/events.gd
+++ src/systems/events.gd
@@ -3,13 +3,10 @@ extends Node
 func _ready() -> void:
 	prints("System ready:", self.name)
 
-func ensure(event: String) -> void:
-	if not has_user_signal(event):
-		prints("Creating event...", event)
-		add_user_signal(event)
-
 func on(event: String, node: Node, method: String, args := []) -> void:
-	ensure(event)
-
-	prints("Binding event...", event, method)
+	_ensure(event)
 	connect(event, node, method)
+
+func _ensure(event: String) -> void:
+	if not has_user_signal(event):
+		add_user_signal(event)
--- src/systems/mappings.gd
+++ src/systems/mappings.gd
@@ -1,5 +1,9 @@
 extends Node
 
+func _ready() -> void:
+	prints("System ready:", self.name)
+	self.pause_mode = PAUSE_MODE_PROCESS
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("game.close"): get_tree().quit()
 	elif Input.is_action_pressed("game.restart"): get_tree().reload_current_scene()
