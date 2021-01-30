eb9d843e404e8e4b21fac9e83b248eea0c3c88ff
Ross Hadden <rosshadden@gmail.com>
2021-01-30 13:36:19 -0500

    Refactored select mode.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/gui/hud.gd
+++ src/gui/hud.gd
@@ -7,9 +7,7 @@ func _ready():
 	events.on("deselected", self, "_onDeselected")
 
 func _onSelected(target: Node):
-	prints("selected", target)
 	selection.text = target.name
 
 func _onDeselected(target: Node):
-	prints("deselected", target)
 	selection.text = ""
--- src/systems/modes/select.gd
+++ src/systems/modes/select.gd
@@ -8,7 +8,10 @@ onready var grid: Grid = modes.get_parent().get_node("grid")
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("game.action"):
 		var data := grid.getAllData(event.position)
-		if not data.has("selectable"): return
+
+		if not data.has("selectable"):
+			deselect()
+			return
 
 		# Check for selectable nodes in order of precedence
 		var node: Node
@@ -18,15 +21,23 @@ func _input(event: InputEvent) -> void:
 			node = data.selectable.buildings
 
 		if not node: return
-		if node == selection: return
 
-		if selection:
-			events.emit_signal("deselected", selection)
-			selection.find_node("selectable").emit_signal("deselected")
-
-		selection = node
-		events.emit_signal("selected", selection)
-		selection.find_node("selectable").emit_signal("selected")
+		select(node)
 
 	if Input.is_action_just_pressed("game.context"):
 		modes.change(modes.states.Move)
+
+func select(node: Node) -> void:
+	if node == selection: return
+
+	deselect()
+
+	selection = node
+	events.emit_signal("selected", selection)
+	selection.find_node("selectable").emit_signal("selected")
+
+func deselect() -> void:
+	if not selection: return
+	events.emit_signal("deselected", selection)
+	selection.find_node("selectable").emit_signal("deselected")
+	selection = null
