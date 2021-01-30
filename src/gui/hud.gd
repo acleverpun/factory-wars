aa834553ce9a0fe8ff560c805569c9282d216773
Ross Hadden <rosshadden@gmail.com>
2021-01-30 16:45:04 -0500

    Hooked up HUD to update modes value.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/gui/hud.gd
+++ src/gui/hud.gd
@@ -1,13 +1,18 @@
 extends CanvasLayer
 
-onready var selection: RichTextLabel = $sections/selectionBar/selectionContainer/selection
+onready var modeValue: RichTextLabel = $sections/modeBar/valueContainer/value
+onready var selectionValue: RichTextLabel = $sections/selectionBar/valueContainer/value
 
-func _ready():
+func _ready() -> void:
+	events.on("mode_changed", self, "_onModeChanged")
 	events.on("selected", self, "_onSelected")
 	events.on("deselected", self, "_onDeselected")
 
-func _onSelected(target: Node):
-	selection.text = target.name
+func _onModeChanged(mode: Node) -> void:
+	modeValue.text = mode.name
 
-func _onDeselected(target: Node):
-	selection.text = ""
+func _onSelected(target: Node) -> void:
+	selectionValue.text = target.name
+
+func _onDeselected(target: Node) -> void:
+	selectionValue.text = ""
