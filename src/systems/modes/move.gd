a25c64225795d4a6a69e13e86e5b6cf2c0c3df76
Ross Hadden <rosshadden@gmail.com>
2021-02-16 18:14:00 -0500

    Only allow moving units to unoccupied cells.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/systems/modes/move.gd
+++ src/systems/modes/move.gd
@@ -10,10 +10,16 @@ func _input(event: InputEvent) -> void:
 		return
 
 	if Input.is_action_just_released("ui_select"):
-		move(self.value, event.position)
+		if isValid(self.value, event.position):
+			move(self.value, event.position)
 		return
 
 func move(entity: Node2D, position: Vector2) -> void:
 	entity.position = grid.snapped(position)
 	self.map.updateEntity(entity)
 	entity.find_node("moving").emit_signal("moved")
+
+func isValid(entity: Entity, position: Vector2) -> bool:
+	var gridPos := grid.toGrid(position)
+	var possibleEntity := self.map.getEntity(gridPos)
+	return possibleEntity == null and possibleEntity != entity
