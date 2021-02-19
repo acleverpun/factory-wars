460b53c686d964629bbfad86e9ef01995b4df215
Ross Hadden <rosshadden@gmail.com>
2021-02-18 21:53:41 -0500

    Populated list data from local variable.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/gui/factoryDialog.gd
+++ src/gui/factoryDialog.gd
@@ -1,4 +1,37 @@
 extends PopupDialog
 
-func _ready():
+var listItems := [
+	{
+		"name": "Foo",
+		"description": "Foooooood.",
+	},
+	{
+		"name": "Bar",
+		"description": "Baaaaaard.",
+	},
+	{
+		"name": "Baz",
+		"description": "Baaaaaaaz.",
+		"enabled": false,
+	},
+]
+
+onready var buyButton: Button = $buy
+onready var description: RichTextLabel = $description
+onready var list: ItemList = $list
+
+func _ready() -> void:
+	_populate()
 	popup()
+
+func _populate() -> void:
+	list.clear()
+
+	for l in len(listItems):
+		var listItem = listItems[l]
+		list.add_item(listItem.name)
+		list.set_item_disabled(l, not listItem.get("enabled", true))
+
+func _onChange(index: int) -> void:
+	if list.is_item_disabled(index): return
+	prints(listItems[index])
