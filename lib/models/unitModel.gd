4251f0111dacd65b97fa74fd7828236f10279e9a
Ross Hadden <rosshadden@gmail.com>
2021-02-22 22:02:32 -0500

    Renamed resources to models to make it far less confusing.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- lib/models/unitModel.gd
+++ lib/models/unitModel.gd
@@ -6,4 +6,4 @@ export(String) var name
 export(String, MULTILINE) var description
 export(bool) var enabled = true
 
-export(Resource) var cost = preload("res://src/resources/cache.tres")
+export(Resource) var cost = preload("res://src/models/cache.tres")
