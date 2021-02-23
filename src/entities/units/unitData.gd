2839053e305e172519d829134802463c173b8ef7
Ross Hadden <rosshadden@gmail.com>
2021-02-22 21:56:55 -0500

    Used cache resource directly instead of a 1:1 node.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/entities/units/unitData.gd
+++ src/entities/units/unitData.gd
@@ -4,5 +4,6 @@ export(PackedScene) var entity = preload("res://src/entities/units/unit.tscn")
 
 export(String) var name
 export(String, MULTILINE) var description
-export(int) var price
 export(bool) var enabled = true
+
+export(Resource) var cost = preload("res://src/resources/cache.tres")
