692962bf3aaf76663f0113088403b136a19ddb4f
Ross Hadden <rosshadden@gmail.com>
2021-02-28 20:17:10 -0500

    Re-added infantry and tanks.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- lib/models/unit.gd
+++ lib/models/unit.gd
@@ -6,4 +6,4 @@ export(String) var name
 export(String, MULTILINE) var description
 export(bool) var enabled = true
 
-export(Cache) var cost
+export(Cache) var cost: Cache
--- lib/models/units/infantry.gd
+++
@@ -1,3 +0,0 @@
-class_name Infantry extends UnitModel
-
-export(String) var name = "Infantry"
