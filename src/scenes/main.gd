8eb87e773374ab7c6f5b3141d261342b86f4f5d1
Ross Hadden <rosshadden@gmail.com>
2021-03-02 16:20:37 -0500

    Migrated to in-editor signals everywhere possible.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,15 +1,7 @@
 extends MarginContainer
 
-onready var buttons = $rows/buttons
-onready var sandboxButton: Button = buttons.get_node("sandbox")
-onready var quitButton: Button = buttons.get_node("quit")
-
-func _ready() -> void:
-	sandboxButton.connect("button_down", self, "_on_press_sandbox")
-	quitButton.connect("button_down", self, "_on_press_quit")
-
-func _on_press_sandbox() -> void:
+func _on_sandbox_pressed() -> void:
 	get_tree().change_scene("res://src/maps/sandbox.tscn")
 
-func _on_press_quit() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
