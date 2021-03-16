6f0444f90bd7c340bdb8f5673a73afb8ee07f14e
Ross Hadden <rosshadden@gmail.com>
2021-03-16 14:36:22 -0400

    Disable built units until next turn.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/systems/modes/place.gd
+++ src/systems/modes/place.gd
@@ -8,11 +8,18 @@ func _ready() -> void:
 	var building: Building = self.data.entity
 	var unit: Unit = self.data.unitScene.instance()
 
+	# set data
 	unit.model = self.data.model
 	unit.commander = map.commander
+
+	# set position
 	unit.position = grid.snapped(building.position) + grid.Down
 
+	# add to scene
 	entities.add_child(unit)
+
+	# disable selection
+	unit.components.selecting.enable(false)
 
 	yield(get_tree(), "idle_frame")
 	self.succeed()
