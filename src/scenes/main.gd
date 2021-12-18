2bad4a196fac568347a4a21062d5d676c928ac67
Ross Hadden <rosshadden@gmail.com>
2021-12-18 16:59:45 -0500

    Added comments for planning.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,8 +1,13 @@
 extends MarginContainer
 
+# const Game = preload("res://src/scenes/game.tscn")
+
+# TODO: ultimately there will be some list of stages or something,
+# and each entry will have its own stage model.
 export(StageModel) var stage
 
 func _on_sandbox_pressed() -> void:
+	# var game: Game = Game.instance()
 	get_tree().change_scene_to(stage.map)
 
 func _on_quit_pressed() -> void:
